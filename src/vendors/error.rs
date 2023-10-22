25658832f4f5910ae964e50f3d25806dc29a0574
MamoruDS <mamoruds.io@gmail.com>
2023-10-23 00:35:02 +0900

    addd: `NvmlWrapperError` for wrapping err from `nvml_wrapper`

--- src/vendors/error.rs
+++ src/vendors/error.rs
@@ -1,7 +1,10 @@
+use nvml_wrapper::error::NvmlError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum BackendError {
     #[error("failed to initialize nvml with nvml_wrapper")]
     NvmlInitError,
+    #[error("failed due to nvml_wrapper error {0}")]
+    NvmlWrapperError(#[from] NvmlError),
 }
