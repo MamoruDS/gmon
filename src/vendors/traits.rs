659c8c85595594aeccadb3915ef8ceea03e55295
MamoruDS <mamoruds.io@gmail.com>
2023-10-23 01:23:09 +0900

    add: trait `GpuProviderInfo` for access prodiver info

--- src/vendors/traits.rs
+++ src/vendors/traits.rs
@@ -1,6 +1,11 @@
 use super::error::BackendError;
 use super::types::{MemoryInfo, PowerInfo, Value};
 
+pub trait GpuProviderInfo<'a> {
+    fn driver_version(&self) -> Result<Value<String>, BackendError>;
+    fn device_count(&self) -> Result<Value<u32>, BackendError>;
+}
+
 pub trait GpuInfo<'a> {
     fn index(&self) -> Result<Value<u32>, BackendError>;
     fn name(&self) -> Result<Value<String>, BackendError>;
